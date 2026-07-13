0a48400c5d44649247dc9b1e3a3d1c3c30242e66
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-13 16:36:35 +0700

    Chore: Create Xcode Project

---
+++ Naendi/Naendi/NaendiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaendiApp.swift
+//  Naendi
+//
+//  Created by Bryan Samuel on 13/07/26.
+//
+
+import SwiftUI
+
+@main
+struct NaendiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
