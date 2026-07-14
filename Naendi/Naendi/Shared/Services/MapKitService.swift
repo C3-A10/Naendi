d0e71bdc1b2a7ff23a614313ce36585814784d67
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-14 14:09:31 +0700

    Feat: add mapkit service, create placeholder for navigation function

--- Naendi/Naendi/Shared/Services/MapKitService.swift
+++ Naendi/Naendi/Shared/Services/MapKitService.swift
@@ -5,3 +5,19 @@
 //  Created by Bryan Samuel on 14/07/26.
 //
 
+import Foundation
+import MapKit
+import Combine
+
+class MapKitService: NSObject {
+    
+    /// Open Apple Maps with navigation from origin to destination
+    func openAppleMapsRoute(from origin:CLLocationCoordinate2D, to destination:CLLocationCoordinate2D) {
+        // TODO: Implement Apple Maps navigation
+        
+        let destinationItem = MKMapItem(placemark: MKPlacemark(coordinate: destination, addressDictionary: nil))
+        destinationItem.openInMaps(launchOptions: [
+            MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+        ])
+    }
+}
