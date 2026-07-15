35207ea7c4870ff940ed77ac3e9bddea5d6dc887
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-16 00:45:48 +0700

    fix: tags ui

--- Naendi/Naendi/Decide/Components/CompareTable.swift
+++ Naendi/Naendi/Decide/Components/CompareTable.swift
@@ -43,11 +43,14 @@ struct CompareCard: View {
                         switch phase {
                             
                         case .success(let image):
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(height: 130)
-                                .clipped()
+                            GeometryReader { geo in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geo.size.width, height: geo.size.height)
+                                    .clipped()
+                            }
+                            .frame(height: 130)
                             
                         case .failure, .empty:
                             placeholderView
@@ -97,7 +100,7 @@ struct CompareCard: View {
                             .font(.system(size: 12, weight: .semibold))
                             .foregroundColor(.gray)
                     }
-                    .padding(.trailing, 4)
+//                    .padding(.trailing, 4)
                     
                     TagView(
                         text: item.vibe,
@@ -157,7 +160,7 @@ struct CompareCard: View {
         var body: some View {
             Text(text)
                 .font(.system(size: 10, weight: .bold))
-                .padding(.horizontal, 8)
+                .padding(.horizontal, 6)
                 .padding(.vertical, 4)
                 .background(backgroundColor)
                 .foregroundColor(textColor)
@@ -186,47 +189,47 @@ struct CompareCard: View {
         }
     }
 }
-
-// MARK: - PREVIEW
-#Preview {
-    CompareTable(
-        placeA: Place(
-            id: "ChIJZWbUpcD71y0RasSgwAJ7TUw_2",
-            nama: "Holden Martabak & Terang Bulan",
-            alamat: "Jl. Kalidami VII No.2, RT.003/RW.10, Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285, Indonesia",
-            latitude: -7.2760381,
-            longitude: 112.7583921,
-            rangeHarga: "Di bawah Rp 25 rb",
-            jamBuka: "12.00-23.00",
-            typeTempat: "Street Food",
-            rating: 4.2,
-            jumlahReview: 115,
-            vibe: "Casual",
-            halal: "yes",
-            halalEvidence: "No Pork No Lard",
-            reviewPositif: "Harga sangat terjangkau untuk mahasiswa, rasanya pas tidak terlalu manis dan adonannya lembut.",
-            reviewNegatif: "Antrean lumayan panjang kalau malam minggu dan tempat parkirnya agak sempit untuk mobil.",
-            imgUrl: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?q=80&w=800&auto=format&fit=crop",
-            reportCount: 12
-        ),
-        placeB: Place(
-            id: "ChIJZWbUpcD71y0RasSgwAJ7TUw_2",
-            nama: "Kopi Joyo",
-            alamat: "Jl. Kalidami VII No.2, RT.003/RW.10, Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285, Indonesia",
-            latitude: -7.2760381,
-            longitude: 112.7583921,
-            rangeHarga: "Di bawah Rp 25 rb",
-            jamBuka: "12.00-23.00",
-            typeTempat: "Street Food",
-            rating: 4.2,
-            jumlahReview: 115,
-            vibe: "Casual",
-            halal: "no",
-            halalEvidence: "No Pork No Lard",
-            reviewPositif: "Harga sangat terjangkau untuk mahasiswa, rasanya pas tidak terlalu manis dan adonannya lembut.",
-            reviewNegatif: "Antrean lumayan panjang kalau malam minggu dan tempat parkirnya agak sempit untuk mobil.",
-            imgUrl: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?q=80&w=800&auto=format&fit=crop",
-            reportCount: 12
+    
+    // MARK: - PREVIEW
+    #Preview {
+        CompareTable(
+            placeA: Place(
+                id: "ChIJZWbUpcD71y0RasSgwAJ7TUw_2",
+                nama: "Holden Martabak & Terang Bulan",
+                alamat: "Jl. Kalidami VII No.2, RT.003/RW.10, Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285, Indonesia",
+                latitude: -7.2760381,
+                longitude: 112.7583921,
+                rangeHarga: "Di bawah Rp 25 rb",
+                jamBuka: "12.00-23.00",
+                typeTempat: "Street Food",
+                rating: 4.2,
+                jumlahReview: 115,
+                vibe: "Casual",
+                halal: "yes",
+                halalEvidence: "No Pork No Lard",
+                reviewPositif: "Harga sangat terjangkau untuk mahasiswa, rasanya pas tidak terlalu manis dan adonannya lembut.",
+                reviewNegatif: "Antrean lumayan panjang kalau malam minggu dan tempat parkirnya agak sempit untuk mobil.",
+                imgUrl: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?q=80&w=800&auto=format&fit=crop",
+                reportCount: 12
+            ),
+            placeB: Place(
+                id: "ChIJZWbUpcD71y0RasSgwAJ7TUw_2",
+                nama: "Kopi Joyo",
+                alamat: "Jl. Kalidami VII No.2, RT.003/RW.10, Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285, Indonesia",
+                latitude: -7.2760381,
+                longitude: 112.7583921,
+                rangeHarga: "Di bawah Rp 25 rb",
+                jamBuka: "12.00-23.00",
+                typeTempat: "Street Food",
+                rating: 4.2,
+                jumlahReview: 115,
+                vibe: "Casual",
+                halal: "no",
+                halalEvidence: "No Pork No Lard",
+                reviewPositif: "Harga sangat terjangkau untuk mahasiswa, rasanya pas tidak terlalu manis dan adonannya lembut.",
+                reviewNegatif: "Antrean lumayan panjang kalau malam minggu dan tempat parkirnya agak sempit untuk mobil.",
+                imgUrl: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?q=80&w=800&auto=format&fit=crop",
+                reportCount: 12
+            )
         )
-    )
-}
+    }
