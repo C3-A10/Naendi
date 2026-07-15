b95dcef78224d555d05f569b260cd4783908ad69
Mas Bowo <C14220311@john.petra.ac.id>
2026-07-15 16:34:30 +0700

    feat: create temporary compareview

--- Naendi/Naendi/Decide/Views/CompareView.swift
+++ Naendi/Naendi/Decide/Views/CompareView.swift
@@ -5,3 +5,24 @@
 //  Created by Bryan Samuel on 14/07/26.
 //
 
+import SwiftUI
+
+struct CompareView: View {
+    let places: [Place]
+    
+    var body: some View {
+        List {
+            ForEach(places) { place in
+                VStack(alignment: .leading) {
+                    Text(place.nama).font(.headline)
+                    Text("Rating: \(place.rating, specifier: "%.1f")")
+                }
+            }
+        }
+        .navigationTitle(Text("Compare"))
+    }
+}
+
+#Preview {
+    // CompareView()
+}
