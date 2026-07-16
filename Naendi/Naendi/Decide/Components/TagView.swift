70dd673294aa677f0dc566a93d690277d4613ca0
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-16 13:44:21 +0700

    chore: refactor components into different files

---
+++ Naendi/Naendi/Decide/Components/TagView.swift
@@ -0,0 +1,28 @@
+//
+//  TagView.swift
+//  Naendi
+//
+//  Created by Bryan Samuel on 16/07/26.
+//
+import SwiftUI
+
+// MARK: - Sub Component Tags
+struct TagView: View {
+    let text: String
+    let backgroundColor: Color
+    let textColor: Color
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: 10, weight: .bold))
+            .padding(.horizontal, 6)
+            .padding(.vertical, 4)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .cornerRadius(8)
+    }
+}
+
+#Preview{
+    TagView(text: "Hello", backgroundColor: Color.red, textColor: Color.green)
+}
