2457641586f3cc57f5439758d5fd47e687f0ba76
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-16 11:36:29 +0700

    feat: connect navigation from result page to compare

--- Naendi/Naendi/Decide/Views/CompareView.swift
+++ Naendi/Naendi/Decide/Views/CompareView.swift
@@ -7,75 +7,35 @@
 import SwiftUI
 
 struct CompareView: View {
+    @Environment(\.dismiss) var dismiss
     
     let placeA: Place
     let placeB: Place
     
     var body: some View {
-        VStack{
+        
+        VStack(spacing: 0) {
+            
+            // Compare Table
             CompareTable(placeA: placeA, placeB: placeB)
-        }
-    }
-    
-}
-
-#Preview {
-    CompareView(placeA: Place(
-        id: "ChIJZWbUpcD71y0RasSgwAJ7TUw_2",
-        nama: "Kopi Joyo",
-        alamat: "Jl. Kalidami VII No.2, RT.003/RW.10, Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285, Indonesia",
-        latitude: -7.2760381,
-        longitude: 112.7583921,
-        rangeHarga: "Di bawah Rp 25 rb",
-        jamBuka: "12.00-23.00",
-        typeTempat: "Street Food",
-        rating: 4.2,
-        jumlahReview: 115,
-        vibe: "Casual",
-        halal: "no",
-        halalEvidence: "No Pork No Lard",
-        reviewPositif: "Harga sangat terjangkau untuk mahasiswa, rasanya pas tidak terlalu manis dan adonannya lembut.",
-        reviewNegatif: "Antrean lumayan panjang kalau malam minggu dan tempat parkirnya agak sempit untuk mobil.",
-        imgUrl: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?q=80&w=800&auto=format&fit=crop",
-        reportCount: 12
-    ), placeB: Place(
-        id: "ChIJZWbUpcD71y0RasSgwAJ7TUw_2",
-        nama: "Holden Martabak & Terang Bulan",
-        alamat: "Jl. Kalidami VII No.2, RT.003/RW.10, Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285, Indonesia",
-        latitude: -7.2760381,
-        longitude: 112.7583921,
-        rangeHarga: "Di bawah Rp 25 rb",
-        jamBuka: "12.00-23.00",
-        typeTempat: "Street Food",
-        rating: 4.2,
-        jumlahReview: 115,
-        vibe: "Casual",
-        halal: "yes",
-        halalEvidence: "No Pork No Lard",
-        reviewPositif: "Harga sangat terjangkau untuk mahasiswa, rasanya pas tidak terlalu manis dan adonannya lembut.",
-        reviewNegatif: "Antrean lumayan panjang kalau malam minggu dan tempat parkirnya agak sempit untuk mobil.",
-        imgUrl: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?q=80&w=800&auto=format&fit=crop",
-        reportCount: 12))
-}
-
-import SwiftUI
-
-struct CompareView: View {
-    let places: [Place]
-    
-    var body: some View {
-        List {
-            ForEach(places) { place in
-                VStack(alignment: .leading) {
-                    Text(place.nama).font(.headline)
-                    Text("Rating: \(place.rating, specifier: "%.1f")")
+            
+            // Button
+            CustomActionButton(
+                text: "Choose this location",
+                backgroundColor: .white,
+                textColor: .black,
+                action: {
+                    print("Location Selected!")
                 }
-            }
+            )
+            .padding(.top, 16)
+            .padding(.bottom, 12)
+            .padding(.horizontal, 24)
         }
-        .navigationTitle(Text("Compare"))
+        Spacer()
     }
 }
 
 #Preview {
-    CompareView(places: Place.dummyData)
+    CompareView(placeA: Place.dummyData[1], placeB: Place.dummyData[2])
 }
