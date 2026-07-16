9206411a0dd798efff6d277e42be442d3dc6632c
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-16 16:06:42 +0700

    feat: add button and card

--- Naendi/Naendi/Decide/Views/DetailPlaceView.swift
+++ Naendi/Naendi/Decide/Views/DetailPlaceView.swift
@@ -11,46 +11,36 @@ import MapKit
 struct DetailPlaceView: View {
     let place: Place
     
+    @State private var viewModel = DecideViewModel()
+    @State private var isComparing: Bool = false
+    @State private var selectedImageURL: URL? = nil
+    
     var body: some View {
         VStack {
-            Button {
-                let coordinate = CLLocationCoordinate2D(
-                    latitude: place.latitude,
-                    longitude: place.longitude
-                )
-                
-                let address = MKAddress(fullAddress: place.alamat, shortAddress: nil)
-                                
-                let destination = MKMapItem(
-                    location: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude),
-                    address: address
-                )
-                
-                destination.name = place.nama
-                
-                destination.openInMaps(launchOptions: [
-                    MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
-                ])
-            } label: {
-                HStack {
-                    Image(systemName: "map.fill")
-                    Text("Buka di Maps")
-                        .fontWeight(.semibold)
+            // Card
+            PlaceResultCardView(
+                place: place,
+                viewModel: viewModel,
+                isComparing: $isComparing,
+                selectedImageURL: $selectedImageURL
+            )
+            
+            // Button
+            CustomActionButton(
+                text: "Go to Destination",
+                backgroundColor: Color(red: 207/255, green: 245/255, blue: 64/255),
+                textColor: .black,
+                action: {
+                    print("Location Selected!")
                 }
-                .foregroundColor(.black)
-                .padding(.vertical, 16)
-                .frame(maxWidth: .infinity)
-                .background(Color("color_green"))
-                .clipShape(Capsule())
-                .padding(.horizontal, 24)
-                .padding(.bottom, 20)
-            }
-            .padding(.horizontal)
+            )
+            .padding(.top, 16)
+            .padding(.bottom, 12)
+            .padding(.horizontal, 24)
         }
-        .navigationTitle(Text(place.nama))
     }
 }
 
 #Preview {
-    //DetailPlaceView()
+    DetailPlaceView(place: Place.dummyData[1])
 }
