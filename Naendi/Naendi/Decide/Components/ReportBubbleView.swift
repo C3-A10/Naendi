a36d9c7fb04c97397f9dc6464f61071bee54a9ee
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-07-21 16:00:22 +0700

    design: fix color on reportcount

--- Naendi/Naendi/Decide/Components/ReportBubbleView.swift
+++ Naendi/Naendi/Decide/Components/ReportBubbleView.swift
@@ -13,19 +13,19 @@ struct ReportBubbleView: View {
     let reportCount: Int
 
     var body: some View {
-        HStack(spacing: -6) {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "exclamationmark.bubble.fill")
+                .font(.system(size: 24))
+                .foregroundColor(.white)
+                .shadow(color: .black.opacity(0.15), radius: 4, x: 0, y: 2)
+
             Text("\(reportCount)")
                 .font(.system(size: 9, weight: .bold))
                 .foregroundColor(.white)
                 .frame(width: 16, height: 16)
-                .background(Color(red: 0.61, green: 0.80, blue: 0.22))
+                .background(.red)
                 .clipShape(Circle())
-                .zIndex(1)
-
-            Image(systemName: "exclamationmark.bubble.fill")
-                .font(.system(size: 20))
-                .foregroundColor(.white)
-                .shadow(color: .black.opacity(0.15), radius: 4, x: 0, y: 2)
+                .offset(x: 6, y: -4)
         }
     }
 }
